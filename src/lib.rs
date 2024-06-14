6288f43055f7eeae7512e839b49db7873458d2b0
falcon71181 <abhaythakur71181@gmail.com>
2024-06-14 17:56:39 +0530

    feat: initializing config files

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 mod config;
 use anyhow::{anyhow, Result};
+use std::fs::{create_dir_all, OpenOptions};
+use std::io::{BufRead, BufReader};
 use users::get_current_username;
 
 pub struct RustPaper {
     config: config::Config,
     config_folder: String,
     wallpapers: Vec<String>,
+    wallpapers_list_file_location: String,
 }
 
 impl RustPaper {
@@ -18,12 +21,25 @@ impl RustPaper {
             .expect("Failed to convert username to string")
             .to_string();
         let config_folder = format!("/home/{}/.config/rust-paper", username);
-        let wallpapers: Vec<String> = vec![];
+        create_dir_all(&config.save_location)?;
+        let wallpapers_list_file_location = format!("{}/wallpapers.lst", config_folder);
+        let wallpapers_list_file = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .open(&wallpapers_list_file_location)?;
+        let buffer_reader = BufReader::new(&wallpapers_list_file);
+        let mut wallpapers: Vec<String> = vec![];
+
+        for line in buffer_reader.lines() {
+            let wallpaper = line?;
+            wallpapers.push(wallpaper);
+        }
 
         Ok(Self {
             config: config,
             config_folder: config_folder,
             wallpapers: wallpapers,
+            wallpapers_list_file_location: wallpapers_list_file_location,
         })
     }
 }
