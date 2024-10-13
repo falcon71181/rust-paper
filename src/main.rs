7b4c8e806849e16bdfed16b97d75e203982d21a7
falcon71181 <abhaythakur71181@gmail.com>
2024-10-13 09:32:42 +0530

    feat: clap command sync support added

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
-use rust_paper::RustPaper;
-use std::{env, fs::File, io::BufReader};
 mod helper;
 mod lock;
 
+use clap::{Parser, Subcommand};
+use rust_paper::RustPaper;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Parser)]
+struct Cli {
+    /// add wallpapers
+    // #[arg(short, long, value_parser)]
+    // add: Option<String>,
+
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Sync wallpapers
+    Sync,
+}
+
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-    let mut rust_paper = RustPaper::new().await.unwrap();
-    rust_paper.sync().await.unwrap();
+    // Parse command-line arguments
+    let cli = Cli::parse();
+
+    // Initialize RustPaper
+    let rust_paper = RustPaper::new().await.unwrap();
+
+    match cli.command {
+        Command::Sync => {
+            // Call the sync method
+            rust_paper.sync().await.unwrap();
+        }
+    }
+
     // println!("{:?}", helper::get_folder_path().join("wallpaper.lock"));
     // println!("{:?}", helper::get_home_location());
 }
